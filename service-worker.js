c6a28491258a858c8d4cc584743b31583dd1b162
Tyrone Valmores <johntyronevalmores@gmail.com>
2019-01-07 20:26:29 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/designhub/precache-manifest.82a979050da9a5187e0124eb1258219b.js"
+  "/designhub/precache-manifest.b7ed638cd888d8150120ac70f6cd5e4a.js"
 );
 
 workbox.clientsClaim();
